71f5a17ceb6100317106521f233b357383a0e705
saurabh chaudhary <saurabh.chaudhary1507@gmail.com>
2026-05-23 18:00:12 +0530

    initial commit

---
+++ src/main/java/com/example/chat_client/model/ChatMessageRequest.java
@@ -0,0 +1,8 @@
+package com.example.chat_client.model;
+
+import lombok.Data;
+
+@Data
+public class ChatMessageRequest {
+    private String message;
+}
---
+++ src/test/java/com/example/chat_client/ChatClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
